f7ad051a2d9e49423c440bb1e1bbb5d3a3c31b89
William.orozco <william.orozco@arandasoft.com>
2024-09-30 20:50:59 -0500

    corregir intentos y generacion de reporte

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { configureAllureAdapterPlugins }  = require('@mmisty/cypress-allure-adapt
 
 
 module.exports = defineConfig({
+  retries: 1, // Reintentos por si el caso de prueba falla.
   e2e: {
     setupNodeEvents(on, config) {
       configureAllureAdapterPlugins(on, config);
